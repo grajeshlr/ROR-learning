a3183de25538b1a13e83bf2f11db00590204fd52
grajeshlr <rajeshprkrld@gmail.com>
2021-05-21 17:21:14 +0530

    Associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210520062815) do
+ActiveRecord::Schema.define(version: 20210521111449) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content",    limit: 65535
     t.string   "tittle",     limit: 255
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
